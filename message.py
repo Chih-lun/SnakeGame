dd44d610f9c9a21270aef930bd0c3113fe500ab8
BlackBaoyu <59338015+BlackBaoyu@users.noreply.github.com>
2021-05-15 13:18:11 +0800

    Update message.py

--- message.py
+++ message.py
@@ -4,7 +4,6 @@ class Message:
     def __init__(self,size):
         self.the_message = pygame.font.SysFont(None,size)
 
-
     def show_text(self,window,context,color,position):
         self.message = self.the_message.render(context,True,color)
         window.blit(self.message,position)
